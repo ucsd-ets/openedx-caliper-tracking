227813ad0f5819ffa4168e30e255d92ba0f50519
danial <danial.malik@arbisoft.com>
2019-02-12 17:51:18 +0500

    Add dist for app

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='openedx-caliper_tracking',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description='Caliper Tracking app can be used to transform edx traditional events into caliper standard events.',
+    long_description=README,
+    url='https://github.com/danialmalik/openedx-caliper_tracking.git',
+    author='Danial Malik',
+    author_email='danialmalik321@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
