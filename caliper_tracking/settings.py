c0ba08bb70cec8105a99655a74414d32357a56b0
danial <danial.malik@arbisoft.com>
2019-02-13 12:08:53 +0500

    change paths in app settings

--- caliper_tracking/settings.py
+++ caliper_tracking/settings.py
@@ -3,9 +3,9 @@ All settings for Caliper Tracking app.
 """
 
 LOGIN_EVENT_EMITTER = 'caliper_tracking.helpers.emit_login_event'
-CALIPER_TRACKING_PROCESSOR = 'openedx.features.caliper_tracking.processor.CaliperProcessor'
+CALIPER_TRACKING_PROCESSOR = 'caliper_tracking.processor.CaliperProcessor'
 
-CALIPER_TRACKING_APP_NAME = 'openedx.features.caliper_tracking'
+CALIPER_TRACKING_APP_NAME = 'caliper_tracking.apps.CaliperTrackingConfig'
 
 CALIPER_TRACKING_BACKENDS = {
     'logger': {
