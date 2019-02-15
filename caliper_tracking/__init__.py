78b45287eb79059246ca4076285336214f8fae00
danial <danial.malik@arbisoft.com>
2019-02-15 15:21:06 +0500

    refactor some code

--- caliper_tracking/__init__.py
+++ caliper_tracking/__init__.py
@@ -1,3 +1,8 @@
+"""
+Apply required settings for caliperization of events.
+
+Add CaliperProcessor to event tracking backends' list.
+"""
 from django.conf import settings as django_settings
 
 from . import processor
@@ -7,19 +12,10 @@ from .settings import CALIPER_TRACKING_BACKENDS, CALIPER_TRACKING_PROCESSOR
 default_app_config = 'caliper_tracking.apps.CaliperTrackingConfig'
 
 
-def _enable_caliper_backends():
-        """
-        Apply required settings for caliperization of events.
+if hasattr(django_settings, 'EVENT_TRACKING_BACKENDS'):
+    django_settings.EVENT_TRACKING_BACKENDS['tracking_logs']['OPTIONS']['processors'] += [
+        {'ENGINE': CALIPER_TRACKING_PROCESSOR}
+    ]
 
-        Add CaliperProcessor to event tracking backends' list.
-        """
-        if hasattr(django_settings, 'EVENT_TRACKING_BACKENDS'):
-            django_settings.EVENT_TRACKING_BACKENDS['tracking_logs']['OPTIONS']['processors'] += [
-                {'ENGINE': CALIPER_TRACKING_PROCESSOR}
-            ]
-
-        if hasattr(django_settings, 'TRACKING_BACKENDS'):
-            django_settings.TRACKING_BACKENDS = CALIPER_TRACKING_BACKENDS
-
-
-_enable_caliper_backends()
+if hasattr(django_settings, 'TRACKING_BACKENDS'):
+    django_settings.TRACKING_BACKENDS = CALIPER_TRACKING_BACKENDS
