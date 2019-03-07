340313c3feb80b9b235b05c727ea65eba3a30412
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2019-03-07 19:18:16 +0500

    FIX: setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -12,18 +12,17 @@ setup(
     version='0.9.0',
     packages=find_packages(),
     include_package_data=True,
-    license='BSD License',
-    description='Caliper Tracking app can be used to transform edx traditional events into caliper standard events.',
+    license='GPL 3.0',
+    description='Caliper Tracking app can be used to transform Open edX tracking events into Caliper standard compliant events.',
     long_description=README,
-    url='https://github.com/danialmalik/openedx-caliper_tracking.git',
-    author='Danial Malik',
-    author_email='danialmalik321@gmail.com',
+    url='https://github.com/ucsd-ets/caliper-tracking',
+    author='UC San Diego',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: GPL 3.0',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
